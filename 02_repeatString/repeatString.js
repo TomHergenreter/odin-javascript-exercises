2a86ec5224255aad77e3aad2d97707030b98d06f
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-07 11:00:17 -0700

    solution for javascript exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if(count > -1){
+    string != 0 ? string = string.repeat(count) : string = '';
+    return string;
+    }
+    return string = "ERROR"
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
