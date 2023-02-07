aef54295b83122fc5ae8cffb1e0b84eacf1e8c3b
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-07 11:18:52 -0700

    Solution for javascript exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    return [...stringToReverse].reverse().join('');
 };
+
+console.log(reverseString('tetsing'));
 
 // Do not edit below this line
 module.exports = reverseString;
