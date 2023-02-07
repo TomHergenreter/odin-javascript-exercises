72a00dfcb4621def1eed2d630bb0e660cab5a001
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-07 15:28:00 -0700

    Solution for remove from array javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+//accept two arguments, one array and multiple strigs spread into a seperate array
+//compare arrays, filter first array for numbers that do not match in second array
+//pass to new array.
+const removeFromArray = function(numberArray, ...numbersToRemove) {
+    const slicedArray = numberArray.filter(element => !numbersToRemove.includes(element))
+    console.log(slicedArray);
+    return slicedArray;
 };
 
 // Do not edit below this line
+//const intersection = array1.filter(element => array2.includes(element));
 module.exports = removeFromArray;
