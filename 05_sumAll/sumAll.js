b62d59ef2c6639f3458d5f313051607becd8ade8
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-07 23:03:45 -0700

    Solution for sumAll javascript exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
 
+
+const sumAll = function(...numbers){
+
+    const lowNum = Math.min(...numbers);
+    const highNum = Math.max(...numbers);
+
+    for (const num of numbers){
+        if(typeof num != 'number' || num < 1){
+            console.log('ERROR');
+            return 'ERROR';
+        }    
+    }
+    
+    if(lowNum === 1){
+        return((highNum * (highNum + 1) / 2))
+    }else{
+    return(Math.floor(((highNum - lowNum) + 1) * (highNum + lowNum) / 2));
+    };   
 };
+
+console.log(sumAll(20, 35));
 
 // Do not edit below this line
 module.exports = sumAll;
