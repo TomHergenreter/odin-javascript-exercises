e83f8ec174fd5b692c10bd86784b7a9b87b2953d
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-08 08:13:12 -0700

    Add study note comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,25 +1,20 @@
 
 
-const sumAll = function(...numbers){
+const sumAll = function(...numbers){ //push numbers to array
 
-    const lowNum = Math.min(...numbers);
-    const highNum = Math.max(...numbers);
-
-    for (const num of numbers){
+    const lowNum = Math.min(...numbers); //get lowest number from numbers array
+    const highNum = Math.max(...numbers); //get highest number from numbers array
+    for (const num of numbers){ //loop through numbers to check for proper data type and positive value
         if(typeof num != 'number' || num < 1){
-            console.log('ERROR');
             return 'ERROR';
         }    
     }
-
     if(lowNum === 1){
-        return((highNum * (highNum + 1) / 2))
+        return((highNum * (highNum + 1) / 2))//gauss sumation to get sum of numbers if lowNum is 1
     }else{
-    return(((highNum - lowNum) + 1) * (highNum + lowNum) / 2);
+        return(((highNum - lowNum) + 1) * (highNum + lowNum) / 2);//modified gauss sumation to get sum if lowNum other than 1
     };   
 };
-
-console.log(sumAll(20, 35));
 
 // Do not edit below this line
 module.exports = sumAll;
