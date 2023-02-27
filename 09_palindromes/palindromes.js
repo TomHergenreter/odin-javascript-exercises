d30df59b89fd975dc56ec4658d158eac9beb6aad
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-27 09:12:40 -0700

    Solution for javascript exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+// const palindromes = function (inputString) {
+//     let parsedString = inputString.toLowerCase().replace(/ |,/g, '');
+//     console.log(parsedString);
+// };
 
+const palindromes = function (inputString) {
+    parsedString = inputString.toLowerCase().match(/[a-z]/g).join('');
+    return parsedString === parsedString.split('').reverse().join('') ? true : false;
 };
 
 // Do not edit below this line
