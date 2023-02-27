be1ffedf551def127997fea90879eac26bcc791a
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-27 08:13:07 -0700

    Solutions for javascript exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  let sum = 0;
+	for(num of numbers) {
+    sum += num;
+  };
+  return sum
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+ if (numbers.length > 0){  
+  return numbers.reduce((total, num) => {return total + num});
+ };
+ return 0;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, num) => {return total * num});
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = num;
+  if(num === 0 || num === 1){
+    return 1;
+  }else{
+    while(num > 2){
+      num --;
+      factorial *= num;
+    }
+    return factorial;
+  }
 };
 
 // Do not edit below this line
