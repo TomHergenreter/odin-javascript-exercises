05b948c2ebd15086e9526981bb74ec21c1626140
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-27 10:18:12 -0700

    Solution for javascript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibonacciNumIndex) {
+    if (fibonacciNumIndex < 0){
+        return 'OOPS';
+    }
+    let fNumSequence = [1,0,0];
+    while(fibonacciNumIndex > 0){
+        fibonacciNumIndex--;
+        fNumSequence[2] = fNumSequence[0] + fNumSequence[1];
+        fNumSequence[0] = fNumSequence[1];
+        fNumSequence[1] = fNumSequence[2];
+    }
+    return fNumSequence[2];
 };
 
 // Do not edit below this line
