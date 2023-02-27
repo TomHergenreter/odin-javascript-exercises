d7cd1ba4f5324f6e56a6234d7636e6a1227d2fb5
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-27 11:58:29 -0700

    Sloution for javascript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(people) {
+    const date = new Date();
+    for(person of people){
+        if(!person.yearOfDeath) person.yearOfDeath = date.getFullYear();
+    }
+    const ages = people.sort(function(person1,person2){
+        const person1Age = person1.yearOfDeath - person1.yearOfBirth;
+        const person2Age = person2.yearOfDeath - person2.yearOfBirth;
+        return person1Age > person2Age ? -1 : 1;
+    });
+    return ages[0];
+    
 };
 
-// Do not edit below this line
 module.exports = findTheOldest;
